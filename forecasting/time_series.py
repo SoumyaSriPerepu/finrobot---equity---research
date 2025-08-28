985d6bdf72007cb13ba87724d7bb3c61eccb2166
Soumya Sri Perepu <155461678+SoumyaSriPerepu@users.noreply.github.com>
2025-08-28 22:22:06 +0530

    Update time_series.py

--- forecasting/time_series.py
+++ forecasting/time_series.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from prophet import Prophet
+
+def forecast_prices(price_df: pd.DataFrame, periods: int = 30) -> pd.DataFrame:
+    df = price_df.rename(columns={"Date": "ds", "Close": "y"})[["ds", "y"]].copy()
+    model = Prophet()
+    model.fit(df)
+    future = model.make_future_dataframe(periods=periods)
+    fcst = model.predict(future)
+    return fcst[["ds", "yhat", "yhat_lower", "yhat_upper"]]
 
